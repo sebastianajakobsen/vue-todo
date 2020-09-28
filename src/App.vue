2a9453d38145cbc0fa1f1a2e4926aa3ae1dc791b
faken09 <seb_jakobsen@live.dk>
2020-09-28 14:12:00 +0200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+
+  <Todo/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+import Todo from "./components/Todo.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todo
   }
 }
 </script>
---
+++ src/components/Todo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        Todo
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'Todo',
+        data() {
+            return {}
+        },
+        components: {},
+        methods: {}
+
+    }
+</script>
