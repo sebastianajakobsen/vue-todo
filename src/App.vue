3cd7fb089d3ee378e01db8775854102c615f0446
faken09 <seb_jakobsen@live.dk>
2020-09-30 13:26:02 +0200

    Composition API with reusing logic

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
 
   <Todo title="Todo App"/>
+  <Test/>
 </template>
 
 <script>
 
 
 import Todo from "./components/Todo.vue";
+import Test from "./components/Test.vue";
 export default {
   name: 'App',
   components: {
+    Test,
     Todo
   }
 }
---
+++ src/components/Test.vue
@@ -0,0 +1,25 @@
+<template>
+    <p>MouseXPosition: {{mouseX}} <br/> MouseYPosition: {{mouseY}}</p>
+</template>
+
+<script>
+
+    import {useMousePosition} from "../functions/useMousePosition";
+
+    export default {
+        name: 'Test',
+
+        components: {},
+        setup() {
+            const {x:mouseX, y:mouseY} = useMousePosition()
+
+            return {
+                mouseX,
+                mouseY
+            }
+        }
+
+
+
+    }
+</script>
