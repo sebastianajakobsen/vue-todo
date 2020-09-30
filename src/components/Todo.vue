f02d4a282f69b6788a9360970cb56ac008a4bcbd
faken09 <seb_jakobsen@live.dk>
2020-09-30 12:51:15 +0200

    rewrote code with vuejs 3 state

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -4,13 +4,13 @@
         <h1 class="text-2xl font-bold">Todo</h1>
 
         <label>
-            <input @keyup.enter="addTodo" v-model="newTodo" type="text"
+            <input @keyup.enter="addTodo" v-model="state.newTodo" type="text"
                    placeholder="New todo..."
                    class="shadow my-2 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
         </label>
 
-        <div v-for="todo in todos" class="flex items-center justify-between my-3">
+        <div v-for="todo in state.todos" class="flex items-center justify-between my-3">
 
             <div>
                 <label>
@@ -27,19 +27,18 @@
         </div>
 
         <hr class="my-3"/>
-
         <p class="text-sm">Items left: ({{itemsLeft}})</p>
     </div>
 </template>
 
 <script>
     import {v4 as uuidv4} from 'uuid';
+    import {reactive, computed} from "vue";
 
     export default {
         name: 'Todo',
-
-        data() {
-            return {
+        setup() {
+            const state = reactive({
                 newTodo: '',
                 todos: [
                     {
@@ -58,36 +57,30 @@
                         completed: true
                     }
                 ]
-            }
-        },
+            })
 
-        computed: {
-            itemsLeft() {
-                return this.todos.filter(todo => !todo.completed).length
-            }
-        },
-        methods: {
-            deleteTodo(id) {
-                this.todos = this.todos.filter(todo => todo.id !== id);
-            },
 
-            addTodo(e) {
+            function addTodo(e) {
                 e.preventDefault();
+                if (state.newTodo.trim()) {
 
-                if (this.newTodo.trim()) {
                     const newTodoObj = {
                         id: uuidv4(),
-                        description: this.newTodo,
+                        description: state.newTodo,
                         completed: false
                     }
-                    this.todos = [...this.todos, newTodoObj];
-                    this.newTodo = '';
+
+                    state.todos.push(newTodoObj);
+                    state.newTodo = '';
                 }
+            }
 
-            },
+            function deleteTodo(id) {
+                state.todos = state.todos.filter(todo => todo.id !== id);
+            }
 
-            toggleCompleted(id) {
-                this.todos = this.todos.map(todo => {
+           function toggleCompleted(id) {
+                state.todos = state.todos.map(todo => {
                     if (todo.id === id) {
                         return {
                             ...todo,
@@ -97,9 +90,78 @@
                     return todo;
                 })
             }
+
+
+            const itemsLeft = computed(() =>  state.todos.filter(todo => !todo.completed).length)
+
+            return {
+                state,
+                addTodo,
+                deleteTodo,
+                toggleCompleted,
+                itemsLeft
+            }
+        },
+
+        data() {
+            return {
+                // newTodo: '',
+                // todos: [
+                //     {
+                //         id: 1,
+                //         description: 'Finish Vue3 todo',
+                //         completed: false
+                //     },
+                //     {
+                //         id: 2,
+                //         description: 'Make food',
+                //         completed: false
+                //     },
+                //     {
+                //         id: 3,
+                //         description: 'Take a nap',
+                //         completed: true
+                //     }
+                // ]
+            }
+        },
+
+        computed: {
+            // itemsLeft() {
+            //     return this.todos.filter(todo => !todo.completed).length
+            // }
+        },
+        methods: {
+            // deleteTodo(id) {
+            //     this.todos = this.todos.filter(todo => todo.id !== id);
+            // },
+
+            // addTodo(e) {
+            //     e.preventDefault();
+            //
+            //     if (this.newTodo.trim()) {
+            //         const newTodoObj = {
+            //             id: uuidv4(),
+            //             description: this.newTodo,
+            //             completed: false
+            //         }
+            //         this.todos = [...this.todos, newTodoObj];
+            //         this.newTodo = '';
+            //     }
+            // },
+
+        //     toggleCompleted(id) {
+        //         this.todos = this.todos.map(todo => {
+        //             if (todo.id === id) {
+        //                 return {
+        //                     ...todo,
+        //                     completed: !todo.completed
+        //                 }
+        //             }
+        //             return todo;
+        //         })
+        //     }
         }
-
-
     }
 </script>
 
